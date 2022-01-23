e7f56704c322fd00cc225a2c584d1d94990330e8
ValentynValikov <the.silhy@gmail.com>
2022-01-23 23:40:39 +0200

    Display posts on main page
    Display all posts from separate "posts.php" file on home page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php require_once 'inc/header.php'; ?>
-
+<?php require_once 'posts.php'; ?>
 <div class="container">
 
 <div>
@@ -14,46 +14,14 @@
             <th></th>
             <th></th>
         </tr>
-        <tr class="table-light">
-            <td><h5>LSB</h5></td>
-            <td>Works.</td>
-            <td>Buggy</td>
-            <td>21:48:19 24-08-2021</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr class="table-light">
-            <td><h5>Bugs and Features</h5></td>
-            <td>Sometimes bugs can become features!</td>
-            <td>Buggy</td>
-            <td>13:48:04 24-08-2021</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr class="table-light">
-            <td><h5>&lt;script&gt;alert (&#039;hi hacker&#039;); &lt;/script&gt;</h5></td>
-            <td>&lt;script&gt;alert (&#039;hi hacker&#039;); &lt;/script&gt;</td>
-            <td>Buggy</td>
-            <td>23:19:29 23-08-2021</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr class="table-light">
-            <td><h5>New page</h5></td>
-            <td>After saving goes to show page</td>
-            <td>admin</td>
-            <td>15:03:35 03-08-2021</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr class="table-light">
-            <td><h5>Ajax oh no</h5></td>
-            <td>Don&#039;t know how to implement ajax requests. Impo</td>
-            <td>admin</td>
-            <td>15:00:44 03-08-2021</td>
-            <td></td>
-            <td></td>
-        </tr>
+        <?php foreach ($posts as $post) : ?>
+            <tr class="table-light">
+                <td><h5><?php echo htmlspecialchars($post['title']); ?></h5></td>
+                <td><?php echo htmlspecialchars($post['description']); ?></td>
+                <td><?php echo htmlspecialchars($post['author']); ?></td>
+                <td><?php echo htmlspecialchars(date('H:i:s d-m-Y', $post['created_at'])); ?></td>
+            </tr>
+        <?php endforeach; ?>
     </table>
 </div>
 
